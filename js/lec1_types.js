1d626a528c62cf6ffb31cd42d67c88bdd967a83d
TimKot <timkot320@gmail.com>
2021-04-19 13:37:37 +0300

    types

---
+++ js/lec1_types.js
@@ -0,0 +1,19 @@
+console.log('Hi JavaScript!');
+
+// Типы данных простые (примитивные)
+/**
+ * String: 'Hello', "hello"
+ * Number: 1324, 42.2
+ * Boolean: true, false
+ * Null: null (typeoff null // object)
+ * Undefined: undefined
+ * Symbol
+ */
+
+// Сложные типы данных
+/**
+ * Object {key: value}
+ * Array: [1, s, 32]
+ * Date // class for date
+ * 
+ */
